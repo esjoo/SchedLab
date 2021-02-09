076649873d63838c0f0d9ae18026237f249c3ddf
esjoo <esjooo@hotmail.com>
2021-02-09 14:40:38 +0100

    Changed connection to remote server

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php 
 
-$host = 'localhost';
-$user = 'root';
-$pswd = '';
-$database = 'schedLab';
+$host = 'sql7.freemysqlhosting.net';
+$user = 'sql7391808';
+$pswd = 'rbq4yR7P4v';
+$database = 'sql7391808';
 $conn = mysqli_connect($host,$user,$pswd,$database); 
 
 // Check connection
