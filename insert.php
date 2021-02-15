0e90b598721a89f6194b1b1204214267fc716876
MagicicadaSxy <2714485368@qq.com>
2021-02-15 07:58:47 +0100

    fuzzy search

--- insert.php
+++ insert.php
@@ -1,56 +1,14 @@
 <!DOCTYPE html>
 <html>
 
-<!--
-<head>
-    <script>
-        function showSite(str)
-        {
-            if (str=="")
-            {
-                document.getElementById("txtHint").innerHTML="";
-                return;
-            } 
-            if (window.XMLHttpRequest)
-            {
-                // IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
-                xmlhttp=new XMLHttpRequest();
-            }
-            else
-            {
-                // IE6, IE5 浏览器执行代码
-                xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-            }
-            xmlhttp.onreadystatechange=function()
-            {
-                if (xmlhttp.readyState==4 && xmlhttp.status==200)
-                {
-                    document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
-                }
-            }
-            xmlhttp.open("GET","query_for_insertChemical.php?q="+str,true);
-            xmlhttp.send();
-            }
-    </script>
-</head>
--->
-
-
-
-
-
-<?php include('header.php');  
- require_once('db.php');
+<?php 
+include 'header.php';  
+include 'db.php';
 
 $sql = "SELECT Type FROM Protocol";
 $result = mysqli_query($conn, $sql);
+include 'closeDB.php';
 ?> 
-
-<link rel="stylesheet" type="text/css" href="bootstrap.css">
-<link rel="stylesheet" type="text/css" href="bootstrap-select.min.css">
-<script src="jquery.min.js"></script>
-<script src="bootstrap.min.js"></script>
-<script src="bootstrap-select.min.js"></script>
 
 <h1>Create a new protocol</h1>
 <div class="form">
@@ -61,11 +19,7 @@ $result = mysqli_query($conn, $sql);
             <input type="text" class="form-control" placeholder="Enter name" id="name" name="name">
         </div>
                 
-        <!-- Procedure -->
-        <div class="form-group">
-            <label for="procedure">Procedure:</label><br>
-            <textarea name="procedure" cols="40" rows="5" placeholder="Insert procedure"></textarea>
-        </div>
+        
 
         <!-- Equipment -->
         <div class="form-group">
@@ -77,59 +31,45 @@ $result = mysqli_query($conn, $sql);
 
                 
         <!-- Chemicals -->
+        
+        <link rel="stylesheet" type="text/css" href="bootstrap-select.min.css">
+        <script src="bootstrap-select.min.js"></script>
 
         <div class="form-group" id="chemicals">
-            <label for="chemical">Chemicals:</label><br>
-            <select class="selectpicker  form-control" id="chemical" name="chemical" style="width:50px;float:left;margin-bottom:5px;" placeholder="Chemical" data-live-search="true"> 
-                 <?php                
-                 $result = mysqli_query($conn,"SELECT SupID, SupName FROM Supplement");
-                 while($row = mysqli_fetch_array($result)){
-                     $catID = row["SupID"];
-                     $catName = row["SupName"];
-                     echo '<option value="$catID">$catName</option>';
-                 }
-                 ?>
+        <label for="chemical">Chemicals:</label><br>
+            <select class="selectpicker" placeholder="Chemical" name="chemical" data-live-search="true">
+            <?php 
+            include 'db.php';               
+            $result = mysqli_query($conn,"SELECT * FROM Supplement");
+            while($row = $result->fetch_assoc()){
+                $catID = $row['SupID'];
+                $catName = $row['SupName'];
+                echo "<option value='$catID'>$catName</option>";
+                echo '<br>';
+            }
+            include 'closeDB.php';
+            ?>
+        
             </select>
-
-            <!--<input type="text" style="width:85%;float:left;margin-bottom:5px;" class="form-control" name="chemical0" placeholder="Chemical" value="">-->
-            <!--<input type="button" class="btn btn-success" name="plus" id="plus" value="+" onclick="addMore(1, $options);"> -->
         </div>
 
-        <script>
-        function addMore(i, opt) {
+        <!-- Procedure -->
+        <div class="form-group">
+            <label for="procedure">Procedure:</label><br>
+            <textarea name="procedure" cols="40" rows="5" placeholder="Insert procedure"></textarea>
+        </div>
 
-            $("#plus").remove();
-
-            var x = opt.entries();
-            for (a of x){
-                option += '<option value='+$catid'>$catname</option>
-            }
-
-            $('#chemicals').append('<div><input type="button" onclick="removeThis(' + i + ');" class="btn btn-danger" name="minus" id="minus' + i + '" value="-"></div>' 
-            + '<div><select name="chemical" style="width:85%;float:left;margin-bottom:5px;"class="form-control" name="chemical0" placeholder="Chemical" value="">' 
-            + '<option value= + '</select>'
-            + '<input type="button" class="btn btn-success" name="plus" id="plus" value="+" onclick="addMore(' + (++i) + ');"></div>');
-        
-            /*$('#chemicals').append('<div><input type="button" onclick="removeThis(' + i + ');" class="btn btn-danger" name="minus" id="minus' + i + '" value="-"></div>' 
-            + '<div><input type="text" style="width:85%;float:left;margin-bottom:5px;" class="form-control" name="chemical' + i + '" placeholder="Chemical" value="">'
-            + '<input type="button" class="btn btn-success" name="plus" id="plus" value="+" onclick="addMore(' + (++i) + ');"></div>');*/
-        }
-
-        function removeThis(j) {
-            $("#chemical" + j).remove();
-            $("#minus" + j).remove();
-        }
-        </script>
-
-        <!-- Submit -->
+        <!-- Submit --> 
         <div class="form-group">
             <button name="submit" type="submit" class="btn btn-primary">Submit</button>
         </div>
     </form>
-    </div>
+</div>
   </section>
+
   <?php 
-  include('closeDB.php');
-  include('footer.php');  ?> 
+ 
+  include('footer.php');  
+  ?> 
 
   </html>
