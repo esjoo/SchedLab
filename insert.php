af6dfc66e79194baef2c98ba2ade8e1fe32d5384
MagicicadaSxy <2714485368@qq.com>
2021-02-18 21:08:06 +0100

    modified:   insert.php 	modified:   query_for_insertChemical.php

--- insert.php
+++ insert.php
@@ -1,15 +1,56 @@
 <!DOCTYPE html>
-<html>
 
+
+
+<html>
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min.js"></script>
+    
+    <style>
+        div{
+            width:450px;
+        }
+        ul {
+            padding:0;
+            margin:2px 5px; 
+            list-style:none; 
+            border:0; 
+            float:left; 
+            width:100%;
+        }
+        li {
+            width:50%; 
+            float:left; 
+            display:inline-block; 
+        }
+        table, input {
+            text-align:left;
+        }
+        table, td, th 
+        {
+            margin:10px 0;
+            padding:2px 10px;
+        }
+        td, th {
+            border:solid 1px #CCC;
+        }
+        button {
+            padding:3px 5px;
+        }
+    </style>
+</head>
+
+<body>
 <?php 
 include 'header.php';  
 include 'db.php';
 
-$sql = "SELECT Type FROM Protocol";
+$sql = "SELECT Type FROM Protocols";
 $result = mysqli_query($conn, $sql);
 include 'closeDB.php';
 ?> 
 
+<body>
 <h1>Create a new protocol</h1>
 <div class="form">
     <form action="insert.inc.php" method='post'>
@@ -36,50 +77,83 @@ include 'closeDB.php';
             <textarea name="procedure" cols="40" rows="5" placeholder="Insert procedure"></textarea>
         </div>
 
-                
-        <!-- Chemicals -->
-        
+
+        <!-- chemicals which have been in the db -->
         <link rel="stylesheet" type="text/css" href="bootstrap-select.min.css">
         <script src="bootstrap-select.min.js"></script>
-
-        
-        <label for="chemical">Chemicals & Dosages:</label>
-        <br>        
-        <div class="form-group form-inline" id="chemicals">           
-                <select class="selectpicker" placeholder="Chemical" name="chemical" data-live-search="true">
-                    <?php 
-                    include 'db.php';               
-                    $result = mysqli_query($conn,"SELECT * FROM Supplement");
-                    while($row = $result->fetch_assoc()){
-                        $catID = $row['SupID'];
-                        $catName = $row['SupName'];
-                        echo "<option value='$catID'>$catName</option>";
-                        echo '<br>';
-                    }
-                    include 'closeDB.php';
-                    ?>       
-                </select>           
-                <input type="number" class="form-control" name="dosage" placeholder="dosage" />
-                
-        </div>
-        <button class="btn btn-primary" id="addLine">ADD</button>
-
-        <script type="text/javascript">
-		/* 新增检测时间 */
-		$("#addLine").click(function() {
 		
-			var htm = "";
-			htm += "<div class='form-group form-inline' id='chemicals'>";
-			htm += "<select class='selectpicker' placeholder='Chemical' name='chemical' data-live-search='true'>";
-			htm += "";
-			htm += "</select>";
-			htm += "<input type='number' class='form-control' name='dosage' placeholder='dosage' />";			
-			htm += "</div>";
+        
+        <div ng-app="chemicals" ng-controller="myController">
+            <label for="chemical">Chemicals & Dosages:</label>
+            <ul>
+                <li>Chemical</li>
+                    <li>
+                        <select ng-model="name" class="selectpicker" placeholder="Chemical" name="chemical" data-live-search="true">
+                        <?php 
+                        include 'db.php';               
+                        $result = mysqli_query($conn,"SELECT * FROM Supplement");
+                        while($row = $result->fetch_assoc()){
+                            $catID = $row['SupID'];
+                            $catName = $row['SupName'];
+                            echo "<option value='$catName'>$catName</option>";
+                            echo '<br>';
+                        }
+                        include 'closeDB.php';
+                        ?>       
+                        </select>
+                    </li>
+            </ul>
+            <ul>
+                <li>Dosage</li>
+                <li><input type="number" ng-model="dosage" class="form-control" name="dosage" placeholder="dosage" /></li>
+            </ul>
+            <ul>
+                <li> </li><li><button ng-click="addRow()" type="button"> Add Row </button></li>
+            </ul>
 
-			$('#chemicals').append(htm);			
-		});	
-	    </script>
-        <br>
+            <!--CREATE A TABLE-->
+            <table> 
+                <tr>
+                    <th>Num</th>
+                        <th>Chemicals</th>
+                            <th>Dosage</th>
+                </tr>
+
+                <tr ng-repeat="chemicals in chemicalArray">
+                    <td><label>{{$index + 1}}</label></td>
+                    <td><label>{{chemicals.name}}</label></td>
+                    <td><label>{{chemicals.dosage}}</label></td>
+                    <td><input type="checkbox" ng-model="chemicals.Remove"/></td>
+                </tr>
+            </table>
+
+            <div>      
+                <button ng-click="removeRow()" type="button">Remove Row</button>
+            </div>
+
+            
+        </div>
+
+
+
+        <!-- Chemicals which haven't been in the db -->  
+        <br>     
+        If you haven't find the chimecals that you need:
+        <div>
+            <table id="main">
+                <th class="head">Chemicals</th>
+                <th class="head">Dosages</th>
+
+                <tr class="alt">
+                    <td><input type="text" placeholder="Enter Value"></td>
+                    <td><input type="number" placeholder="Enter Value"></td>
+                </tr>
+            </table>
+            <input type="button" value="Add New Row" onclick="addRow();" id="rowButton" />        
+        </div>
+
+
+
 
         <!-- Submit --> 
         <div class="form-group">
@@ -87,10 +161,67 @@ include 'closeDB.php';
         </div>
     </form>
 </div>
-  </section>
+</body>
 
-  <?php 
-  include('footer.php');  
-  ?> 
 
-  </html>
+
+
+
+
+
+
+
+
+<!--The Controller-->
+<script>
+    var app = angular.module('chemicals', []);
+    app.controller('myController', function ($scope) {
+
+        // JSON ARRAY TO POPULATE TABLE.
+        $scope.chemicalArray =[];
+
+        // GET VALUES FROM INPUT BOXES AND ADD A NEW ROW TO THE TABLE.
+        $scope.addRow = function () {
+            if ($scope.name != undefined && $scope.dosage != undefined) {
+                var chemical = [];
+                chemical.name = $scope.name;
+                chemical.dosage = $scope.dosage;
+
+                $scope.chemicalArray.push(chemical);
+
+                // CLEAR TEXTBOX.
+                $scope.name = null;
+                $scope.dosage = null;
+            }
+        };
+
+        // Remove selected rows from table.
+        $scope.removeRow = function () {
+            var arrMovie = [];
+            angular.forEach($scope.chemicalArray, function (value) {
+                if (!value.Remove) {
+                    arrMovie.push(value);
+                }
+            });
+            $scope.chemicalArray = arrMovie;
+        };
+
+    });
+</script>
+
+<script>
+function addRow() {
+    var table = document.getElementById("main");
+	var rws = table.rows;
+	var cols = table.rows[0].cells.length;
+    var row = table.insertRow(rws.length);
+	var cell;
+	for(var i=0;i<cols;i++){
+		cell = row.insertCell(i);
+		cell.innerHTML = '<input type="text" placeholder="Enter Value">';
+	}
+}
+</script>
+
+
+</html>
--- query_for_insertChemical.php
+++ query_for_insertChemical.php
@@ -1,114 +1,43 @@
 <!-- Chemicals -->
-
-<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+        
 <link rel="stylesheet" type="text/css" href="bootstrap-select.min.css">
-<script src="jquery.min.js"></script>
-
 <script src="bootstrap-select.min.js"></script>
-<script src="bootstrap.min.js"></script>
 
-<div class="form-group" id="chemicals">
-    <label for="chemical">Chemicals:</label><br>
-    <select class="selectpicker" placeholder="Chemical" data-live-search="true">
-    <?php 
-    include 'db.php';               
-    $result = mysqli_query($conn,"SELECT * FROM Supplement");
-    while($row = $result->fetch_assoc()){
-        $catID = $row['SupID'];
-        $catName = $row['SupName'];
-        echo "<option value='$catID'>$catName</option>";
-        echo '<br>';
-    }
-    include 'closeDB.php';
-    ?>
         
-    </select>
-</div>
+        <label for="chemical">Chemicals & Dosages:</label>
+        <br>        
+        <div class="form-group form-inline" id="chemicals">           
+                <select class="selectpicker" placeholder="Chemical" name="chemical" data-live-search="true">
+                    <?php 
+                    include 'db.php';               
+                    $result = mysqli_query($conn,"SELECT * FROM Supplement");
+                    while($row = $result->fetch_assoc()){
+                        $catID = $row['SupID'];
+                        $catName = $row['SupName'];
+                        echo "<option value='$catID'>$catName</option>";
+                        echo '<br>';
+                    }
+                    include 'closeDB.php';
+                    ?>       
+                </select>           
+                <input type="number" class="form-control" name="dosage" placeholder="dosage" />
+                
+        </div>
+        <button class="btn btn-primary" id="addLine">ADD</button>
 
-<div class="row">
-            <div class="form-inline">
-                <input type="text" class="form-control" placeholder="关键字" />
-                <input type="text" class="form-control" placeholder="hahaha" />
-                <button class="btn btn-info" type="button" title="ADD" id="AddLine" onclick="AddLine(this)" disabled><span class="glyphicon glyphicon-plus"></span></button>
-            </div>
- </div>
+        <script type="text/javascript">
+		/* 新增检测时间 */
+		$("#addLine").click(function() {
+		
+			var htm = "";
+			htm += "<div class='form-group form-inline' id='chemicals'>";
+			htm += "<select class='selectpicker' placeholder='Chemical' name='chemical' data-live-search='true'>";
+			htm += "";
+			htm += "</select>";
+			htm += "<input type='number' class='form-control' name='dosage' placeholder='dosage' />";			
+			htm += "</div>";
 
-<script>
-//添加中心机IP输入框项 
-function AddLine(obj){ 
-    html = '<div class="form-inline">'+ 
-          '<input type="text" class="form-control" placeholder="关键字" />'+ 
-          '<input type="text" class="form-control" placeholder="hahaha" />'+ 
-          '<span class="input-group-btn">'+ 
-                '<button class="btn btn-info" type="button" title="MINUS" id="DelLine"><span class="glyphicon glyphicon-minus"></span></button>'+ 
-          '</span>'+ 
-        '</div>' 
-    obj.insertAdjacentHTML('beforebegin',html) 
-  } 
-
-  $(document).on('click','#DelLine',function(){ 
-  var el = this.parentNode.parentNode 
-  var centerIp = $(this).parent().parent().find('#ipInput').val() 
-  alertify.confirm('您确定要删除选中的命令？', 
-  function(e){ 
-    if(e){ el.parentNode.removeChild(el)}})}) 
-</script>
-
-
-
-
-
-
-<script>
-        function addMore(i, opt) {
-
-            $("#plus").remove();
-
-            var x = opt.entries();
-            for (a of x){
-                option += '<option value='+$catid'>$catname</option>
-            }
-
-            $('#chemicals').append('<div><input type="button" onclick="removeThis(' + i + ');" class="btn btn-danger" name="minus" id="minus' + i + '" value="-"></div>' 
-            + '<div><select name="chemical" style="width:85%;float:left;margin-bottom:5px;"class="form-control" name="chemical0" placeholder="Chemical" value="">' 
-            + '<option value= + '</select>'
-            + '<input type="button" class="btn btn-success" name="plus" id="plus" value="+" onclick="addMore(' + (++i) + ');"></div>');
-        
-            /*$('#chemicals').append('<div><input type="button" onclick="removeThis(' + i + ');" class="btn btn-danger" name="minus" id="minus' + i + '" value="-"></div>' 
-            + '<div><input type="text" style="width:85%;float:left;margin-bottom:5px;" class="form-control" name="chemical' + i + '" placeholder="Chemical" value="">'
-            + '<input type="button" class="btn btn-success" name="plus" id="plus" value="+" onclick="addMore(' + (++i) + ');"></div>');*/
-        }
-
-        function removeThis(j) {
-            $("#chemical" + j).remove();
-            $("#minus" + j).remove();
-        }
-        </script>    
-
-<div class="input-group" id="centerIpGroup"> 
-  <label class="input-group-addon" id="basic-addon5">中心机IP:</label>   
-  <button class="btn btn-info" type="button" data-toggle="tooltip" title="新增" id="addCenterIpGrpBtn" onclick="addCenterIpGrp(this)" disabled><span class="glyphicon glyphicon-plus"></span></button>    
-</div> 
-
-<script>
-
-function addCenterIpGrp(obj){ 
-    html = '<div class="input-group centerIp">'+ 
-          '<label class="input-group-addon">IP：</label>'+ 
-          '<input type="text" class="form-control" id="ipInput">'+ 
-          '<label class="input-group-addon">注释：</label>'+ 
-          '<input type="text" class="form-control" id="descInput">'+ 
-          '<span class="input-group-btn">'+ 
-                '<button class="btn btn-info" type="button" data-toggle="tooltip" title="删除" id="delCenterIpGrp"><span class="glyphicon glyphicon-minus"></span></button>'+ 
-          '</span>'+ 
-        '</div>' 
-    obj.insertAdjacentHTML('beforebegin',html) 
-  } 
-
-  $(document).on('click','#delCenterIpGrp',function(){ 
-  var el = this.parentNode.parentNode 
-  var centerIp = $(this).parent().parent().find('#ipInput').val() 
-  alertify.confirm('您确定要删除选中的命令？', 
-  function(e){ 
-    if(e){ el.parentNode.removeChild(el)}})}) 
-</script>
+			$('#chemicals').append(htm);			
+		});	
+	    </script>
+        <br>
