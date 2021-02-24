a9e9138105421a3d97d14493edcc94629c8f73a2
MagicicadaSxy <2714485368@qq.com>
2021-02-24 23:09:03 +0100

    modified:   insert.php 	modified:   showProtocol.php

--- showProtocol.php
+++ showProtocol.php
@@ -8,16 +8,13 @@
     <?php 
         include 'header.php';
         include "db.php";
-        //$name = mysqli_real_escape_string($conn, $_POST['name']);
-        //$procedure = mysqli_real_escape_string($conn, $_POST['procedure']);
-        //$equipment = mysqli_real_escape_string($conn, $_POST['equipment']);
+
         $name = $_POST['name'];
         $procedure = $_POST['procedure'];
         $equipment = $_POST['equipment'];
         $chemicals = $_POST['chemicals'];
         $dosages = $_POST['dosages'];
-        $NEWchemicals = $_POST['NEWchemicals'];
-        $NEWdosages = $_POST['NEWdosages'];  
+ 
         include "closeDB.php";
     ?>
 
@@ -55,15 +52,6 @@
                       </tr>";
             };
 
-            $cheLen1 = count($NEWchemicals);
-            for($i=0;$i<$cheLen1;$i++){
-                $c = $NEWchemicals[$i];
-                $d = $NEWdosages[$i];
-                echo "<tr>
-                        <td>{$c}</td>
-                        <td>{$d}</td>
-                      </tr>";
-            };
 
             echo "</table>";
 
