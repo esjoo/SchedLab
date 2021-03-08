01e0d4fac98cbed76a89bf811a1388aaf7712cef
MagicicadaSxy <2714485368@qq.com>
2021-03-08 22:51:49 +0100

    the first uncomplete version

---
+++ email.php
@@ -0,0 +1,40 @@
+<!DOCTYPE>
+  
+<html>
+<head>
+    <title>NETTUTS > Sign up</title>
+    <link href="css/style.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+    <!-- start header div -->
+    <div id="header">
+        <h3>NETTUTS > Sign up</h3>
+    </div>
+    <!-- end header div -->  
+      
+    <!-- start wrap div -->  
+    <div id="wrap">
+          
+        <!-- start php code -->
+          
+        <!-- stop php code -->
+      
+        <!-- title and description -->   
+        <h3>Signup Form</h3>
+        <p>Please enter your name and email addres to create your account</p>
+          
+        <!-- start sign up form -->  
+        <form action="" method="post">
+            <label for="name">Name:</label>
+            <input type="text" name="name" value="" />
+            <label for="email">Email:</label>
+            <input type="text" name="email" value="" />
+              
+            <input type="submit" class="submit_button" value="Sign up" />
+        </form>
+        <!-- end sign up form -->
+          
+    </div>
+    <!-- end wrap div -->
+</body>
+</html>
