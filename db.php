e18c1459adc5f8594d265a968565b72f8bed27cb
LinneaGG <linnea.good@hotmail.com>
2021-03-08 10:26:03 +0100

    adds link to chemical consumption

--- db.php
+++ db.php
@@ -1,16 +1,17 @@
 <?php
-$hostname = "127.0.0.1";
+$hostname = "localhost";
 $username = "root";
 $password = "";
-$dbname = "movdb";
+$dbname = "project";
 $conn = mysqli_connect($hostname, $username, $password,$dbname);
 
-
+/*
 $host = '127.0.0.1';
 $user = 'root';
 $pswd = '';
 $database = 'movdb';
 $conn = mysqli_connect($host,$user,$pswd,$database); 
+*/
 
 // Check connection
 if (mysqli_connect_errno()) {
