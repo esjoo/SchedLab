c4efdd4fcca0baa27da4619c20dad8ffde324682
MagicicadaSxy <2714485368@qq.com>
2021-03-09 15:57:20 +0100

    updata the mail function

--- email.php
+++ email.php
@@ -2,10 +2,13 @@
   
 <html>
 <head>
-    <title>NETTUTS > Sign up</title>
-    <link href="css/style.css" type="text/css" rel="stylesheet" />
+    <title>Sign up</title>
+    
 </head>
 <body>
+    <?php
+        include "db.php";
+    ?>
     <!-- start header div -->
     <div id="header">
         <h3>NETTUTS > Sign up</h3>
@@ -16,12 +19,25 @@
     <div id="wrap">
           
         <!-- start php code -->
+        <?php
+  
+            if(isset($_POST['name']) && !empty($_POST['name']) AND isset($_POST['email']) && !empty($_POST['email'])){
+                // Form Submited
+            }       
+              
+        ?>
           
         <!-- stop php code -->
       
         <!-- title and description -->   
         <h3>Signup Form</h3>
         <p>Please enter your name and email addres to create your account</p>
+
+        <?php 
+            if(isset($msg)){  // Check if $msg is not empty
+                echo '<div class="statusmsg">'.$msg.'</div>'; // Display our message and wrap it with a div with the class "statusmsg".
+            } 
+        ?>
           
         <!-- start sign up form -->  
         <form action="" method="post">
