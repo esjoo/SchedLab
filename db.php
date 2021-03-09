33d00102c52539a4617ae8b09376872ca369422c
MagicicadaSxy <2714485368@qq.com>
2021-03-09 15:58:12 +0100

    upload for testing

--- db.php
+++ db.php
@@ -5,6 +5,14 @@ $password = "";
 $dbname = "movdb";
 $conn = mysqli_connect($hostname, $username, $password,$dbname);
 
+/*
+$hostname = "217.208.61.188";
+$username = "imssnowu";
+$password = "LioMi6009";
+$dbname = "imssnowwhite";
+$conn = mysqli_connect($hostname, $username, $password,$dbname);
+*/
+
 // Check connection
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
