03eaa8ece7e9f3444e81d8f022dcb26e4768f7d9
esjoo <esjooo@hotmail.com>
2021-03-15 11:50:22 +0100

    started working on login UX

--- scripts/login.js
+++ scripts/login.js
@@ -2,7 +2,7 @@ function showLogin() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const loginstate = urlParams.get('state');
-    
+    const calstate = urlParams.get('c');
     if(loginstate=='S') {
         console.log(loginstate);
         $('#login').modal('show');
@@ -13,4 +13,14 @@ function showLogin() {
 
         }
     }
+
+    if(calstate=='S') {
+        console.log('c');
+        
+    } else if (calstate=='F'){
+        $('#newExperiment').modal('show');
+        var modal = document.getElementById('newExperiment')
+        document.getElementById('newExpFeedback').text('Input failed')
+    }
+
     
